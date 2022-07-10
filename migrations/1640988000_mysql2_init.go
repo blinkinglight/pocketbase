e442c19354e0b9e11ae91df0518eaa621a4360bf
Mindaugas Žvirblis <mz@9g.lt>
2022-07-10 15:29:35 +0300

    migrations....

--- migrations/1640988000_mysql2_init.go
+++ migrations/1640988000_mysql2_init.go
@@ -29,18 +29,20 @@ func init() {
 	AppMigrations.Register(func(db dbx.Builder) error {
 		_, tablesErr := db.NewQuery(`
 			CREATE TABLE {{_admins}} (
-				id              TEXT PRIMARY KEY,
+				id              TEXT NOT NULL,
 				avatar          INTEGER DEFAULT 0 NOT NULL,
 				email           TEXT UNIQUE NOT NULL,
 				tokenKey        TEXT UNIQUE NOT NULL,
 				passwordHash    TEXT NOT NULL,
 				lastResetSentAt TEXT DEFAULT '' NOT NULL,
 				created         TEXT DEFAULT '' NOT NULL,
-				updated         TEXT DEFAULT '' NOT NULL
+				updated         TEXT DEFAULT '' NOT NULL,
+				PRIMARY KEY (id)
+				
 			);
 
 			CREATE TABLE {{_users}} (
-				id                     TEXT PRIMARY KEY,
+				id                     TEXT NOT NULL,
 				verified               BOOLEAN DEFAULT FALSE NOT NULL,
 				email                  TEXT UNIQUE NOT NULL,
 				tokenKey               TEXT UNIQUE NOT NULL,
@@ -48,11 +50,12 @@ func init() {
 				lastResetSentAt        TEXT DEFAULT '' NOT NULL,
 				lastVerificationSentAt TEXT DEFAULT '' NOT NULL,
 				created                TEXT DEFAULT '' NOT NULL,
-				updated                TEXT DEFAULT '' NOT NULL
+				updated                TEXT DEFAULT '' NOT NULL,
+				PRIMARY KEY (id)
 			);
 
 			CREATE TABLE {{_collections}} (
-				id         TEXT PRIMARY KEY,
+				id         TEXT NOT NULL,
 				system     BOOLEAN DEFAULT FALSE NOT NULL,
 				name       TEXT UNIQUE NOT NULL,
 				schema     JSON DEFAULT '[]' NOT NULL,
@@ -62,15 +65,17 @@ func init() {
 				updateRule TEXT DEFAULT NULL,
 				deleteRule TEXT DEFAULT NULL,
 				created    TEXT DEFAULT '' NOT NULL,
-				updated    TEXT DEFAULT '' NOT NULL
+				updated    TEXT DEFAULT '' NOT NULL,
+				PRIMARY KEY (id)
 			);
 
 			CREATE TABLE {{_params}} (
-				id      TEXT PRIMARY KEY,
+				id      TEXT NOT NULL,
 				key     TEXT UNIQUE NOT NULL,
 				value   JSON DEFAULT NULL,
 				created TEXT DEFAULT '' NOT NULL,
-				updated TEXT DEFAULT '' NOT NULL
+				updated TEXT DEFAULT '' NOT NULL,
+				PRIMARY KEY (id)
 			);
 		`).Execute()
 		if tablesErr != nil {
